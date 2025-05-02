d3ad77677f3863032f1d86653c7059a573fdff58
Ricardo B. A. Xavier <117283984+mr-manrdrack@users.noreply.github.com>
2025-05-02 15:11:20 -0300

    Update and rename App.java to Main.java

--- src/main/java/br/upe/analisandoLog/Main.java
+++ src/main/java/br/upe/analisandoLog/Main.java
@@ -3,7 +3,7 @@ package br.upe.analisandoLog;
 /**
  * Hello world!
  */
-public class App {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
